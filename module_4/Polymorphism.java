828266b6e06a342836192faca741b6951dc8fdfd
Scott Shannon <scott.a.shannon@outlook.com>
2022-04-05 16:22:26 -0700

    polymorphism example for discussion post

---
+++ module_4/Polymorphism.java
@@ -0,0 +1,29 @@
+// package module_4;
+
+// class Polymorphism {
+//     public void done() {
+//       System.out.println("The sandwich has ... ");
+//     }
+
+//   }
+//   class PeanutButter extends Polymorphism {
+//       public void done() {
+//         System.out.println("Chonky jiffy spread on one side ");
+//     }
+//   }
+//   class Jelly extends Polymorphism {
+//     public void done() {
+//         System.out.println("Grape jelly spread on the other ");
+//     }
+//   }
+//   class Main {
+//     public static void main(String[] args) {
+//       Polymorphism mySandwich = new Polymorphism();  // Create a sandwich object
+//       Polymorphism myPeanutButter = new PeanutButter();  // Create a Peanut butter object
+//       Polymorphism myJelly = new Jelly();  // Create a Jelly object
+//       mySandwich.done();
+//       myPeanutButter.done();
+//       mySandwich.done();
+//       myJelly.done();
+//     }
+//   }
