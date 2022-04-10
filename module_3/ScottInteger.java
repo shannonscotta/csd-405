0fa8a6a473e78ea25a8abafc0a93103e6d4a2642
Scott Shannon <scott.a.shannon@outlook.com>
2022-04-09 19:16:26 -0700

    clean up code

--- module_3/ScottInteger.java
+++ module_3/ScottInteger.java
@@ -1,33 +1,28 @@
+// // Scott Shannon            Assignment 3                9 April 2022
+// // The purpose of this code is write getters, setters, and methods that take both Integers and ints and then test all of them using 3 instances
+public class ScottInteger {
 
-
-/*
-Write test code that creates three instances, two with the same value, and one with a different value.
-Write test code that ensures all methods function correctly.
-*/
-
-public class JoshInteger {
-
-  private int josh;
+  private int scott;
   // An int data field titled with your first name.
 
-  public JoshInteger(int josh) {
+  public ScottInteger(int scott) {
     // A constructor that takes and sets the initial value.
-    this.josh = josh;
+    this.scott = scott;
   }
 
-  public int getJosh() {
+  public int getScott() {
     // getter
-    return josh;
+    return scott;
   }
 
-  public void setJosh(int josh) {
+  public void setScott(int scott) {
     // setter
-    this.josh = josh;
+    this.scott = scott;
   }
 
   // Non-Static methods titled isEven()
   public boolean isEven() {
-    if (josh % 2 == 0) {
+    if (scott % 2 == 0) {
       return true;
     } else {
       return false;
@@ -54,7 +49,7 @@ public class JoshInteger {
 
   // Non-Static methods titled isOdd()
   public boolean isOdd() {
-    if (josh % 2 != 0) {
+    if (scott % 2 != 0) {
       return true;
     } else {
       return false;
@@ -81,8 +76,8 @@ public class JoshInteger {
 
   // Non-Static methods titled isPrime()
   public boolean isPrime() {
-    for (int i = 2; i <= josh / 2; i++) {
-      if (josh % i == 0) {
+    for (int i = 2; i <= scott / 2; i++) {
+      if (scott % i == 0) {
         return false;
       }
     }
@@ -112,7 +107,7 @@ public class JoshInteger {
   // The method equals(int) which returns true if int values are equal and false
   // if they are not.
   public boolean equals(int num) {
-    if (num == josh) {
+    if (num == scott) {
       return true;
     } else {
       return false;
@@ -122,7 +117,7 @@ public class JoshInteger {
   // The method equals(Integer) which returns true if int values are equal and
   // false if they are not.
   public boolean equals(Integer num) {
-    if (num == josh) {
+    if (num == scott) {
       return true;
     } else {
       return false;
@@ -130,20 +125,21 @@ public class JoshInteger {
   }
 
   public static void main(String[] args) {
-  
-    JoshInteger a = new JoshInteger(3);
+  // main method containing all instances and tests
+
+    ScottInteger a = new ScottInteger(3);
     System.out.println("Test if 3 is a prime number. Should be true: "+a.isPrime());
     System.out.println("Test if 3 is an odd number. Should be true: "+a.isOdd());
 	  System.out.println("Test if 3 is an even number. Should be false: "+a.isEven());
     System.out.println("Test if the integer 42 equals 3. Should be false: "+a.equals(42));
     
-    JoshInteger b = new JoshInteger(3);
+    ScottInteger b = new ScottInteger(3);
     System.out.println("Test if 3 is a prime Integer. Should be true: "+b.isPrime());
     System.out.println("Test if 3 equals 3. Should be true: "+b.equals(3));
     System.out.println("Test if 3 is an odd Integer. Should be true: "+b.isOdd());
 	  System.out.println("Test if 3 is an even Integer. Should be false: "+b.isEven());
     
-    JoshInteger c = new JoshInteger(7);
+    ScottInteger c = new ScottInteger(7);
     System.out.println("Test if 7 is a prime Integer. Should be true: "+c.isPrime());
   }
 }
