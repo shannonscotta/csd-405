e20a4fa7b82cfeed7c4640787704d462b6e617d4
Scott Shannon <scott.a.shannon@outlook.com>
2022-05-15 12:26:21 -0700

    finish mod 11 assignment

--- src/main/java/com/example/test/ShapesAndColors.java
+++ src/main/java/com/example/test/ShapesAndColors.java
@@ -1,6 +1,5 @@
-//Write a program that displays various figures such as a Circle, a Rectangle, or an Ellipse.
-//Include radio buttons selections for changing the display figure to the one selected.
-//Include a checkbox for filling and clearing the displayed figure with a random color.
+// scott shannon                15 May 2022             Module 11 assignment
+// The purpose of this code is to display shapes from radio button selections and then change the color based on checkboxes
 
 package com.example.test;
 
@@ -40,63 +39,56 @@ public class ShapesAndColors extends javafx.application.Application {
                 // radio button for circle
                 RadioButton circleRadioButton = new RadioButton("Circle");
 
+                //set circle radio button to toggle group
                 circleRadioButton.setToggleGroup(shapeRadioButtons);
 
-                //
+                //set circle to false 
                 circleOne.setVisible(false);
 
-                // rectangle
+
+                // create rectangle same steps as circle
                 Rectangle rectangleOne = new Rectangle(50, 50);
-
                 rectangleOne.setFill(Color.color(Math.random(), Math.random(), Math.random()));
-
                 Label rectangleLabel = new Label("", rectangleOne);
-
                 rectangleLabel.setContentDisplay(ContentDisplay.BOTTOM);
-
                 rectangleLabel.setTextFill(Color.BLACK);
-
                 rectangleOne.setVisible(false);
-
                 RadioButton rectangleRadioButton = new RadioButton("Rectangle");
-
                 rectangleRadioButton.setToggleGroup(shapeRadioButtons);
 
-                // ellipse
+                // create ellipse same steps as circle
                 Ellipse ellipseOne = new Ellipse(25, 25);
-
                 ellipseOne.setFill(Color.color(Math.random(), Math.random(), Math.random()));
-
                 Label ellipseLabel = new Label("", ellipseOne);
-
                 ellipseLabel.setContentDisplay(ContentDisplay.BOTTOM);
-
                 ellipseLabel.setTextFill(Color.BLACK);
-
                 ellipseOne.setVisible(false);
-
                 RadioButton ellipseRadioButton = new RadioButton("Ellipse");
-
                 ellipseRadioButton.setToggleGroup(shapeRadioButtons);
 
+
+                // create checkboxes 
                 CheckBox redCheckBox = new CheckBox("RED");
-
                 CheckBox blueCheckBox = new CheckBox("BLUE");
-
                 CheckBox greenCheckbox = new CheckBox("GREEN");
+                CheckBox randomCheckbox = new CheckBox("RANDOM");
 
+                // create Hbox and args to paneTwo
                 HBox paneTwo = new HBox();
-                paneTwo.getChildren().addAll(redCheckBox, blueCheckBox, greenCheckbox);
+                paneTwo.getChildren().addAll(redCheckBox, blueCheckBox, greenCheckbox, randomCheckbox);
 
+                // create VBox and args to pane
                 VBox pane = new VBox();
                 pane.getChildren().addAll(circleRadioButton, circleLabel, rectangleRadioButton, rectangleLabel,
                                 ellipseRadioButton, ellipseLabel, paneTwo);
 
+                // create, set, and display scene
                 Scene scene = new Scene(pane, 600, 300);
                 primaryStage.setTitle("Module 11 Assignment");
                 primaryStage.setScene(scene);
                 primaryStage.show();
 
+                // event listener for redCheckBox
                 redCheckBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
                         @Override
                         public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue,
@@ -109,6 +101,7 @@ public class ShapesAndColors extends javafx.application.Application {
 
                                         blueCheckBox.setSelected(false);
                                         greenCheckbox.setSelected(false);
+                                        randomCheckbox.setSelected(false);
                                 }
                         }
                 });
@@ -125,6 +118,7 @@ public class ShapesAndColors extends javafx.application.Application {
 
                                         redCheckBox.setSelected(false);
                                         greenCheckbox.setSelected(false);
+                                        randomCheckbox.setSelected(false);
                                 }
                         }
                 });
@@ -141,10 +135,30 @@ public class ShapesAndColors extends javafx.application.Application {
 
                                         blueCheckBox.setSelected(false);
                                         redCheckBox.setSelected(false);
+                                        randomCheckbox.setSelected(false);
                                 }
                         }
                 });
 
+                randomCheckbox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+                        @Override
+                        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue,
+                                        Boolean newValue) {
+
+                                if (randomCheckbox.isSelected()) {
+                                        circleOne.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+                                        rectangleOne.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+                                        ellipseOne.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+
+                                        blueCheckBox.setSelected(false);
+                                        redCheckBox.setSelected(false);
+                                        greenCheckbox.setSelected(false);
+
+                                }
+                        }
+                });
+
+                // event listener for circle radio button
                 circleRadioButton.selectedProperty().addListener(new ChangeListener<Boolean>() {
                         @Override
                         public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue,
@@ -186,6 +200,7 @@ public class ShapesAndColors extends javafx.application.Application {
 
         }
 
+        // launch args
         public static void main(String args[]) {
                 launch(args);
         }
